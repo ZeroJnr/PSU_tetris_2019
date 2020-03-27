a6bb45bfe5d5d888db8dcbca8806df3f65d772d9
Liam <liam.esteffe@epitech.eu>
2020-03-27 02:19:44 +0100

    feat new xterm fix

--- src/parsing/parse_debug_mode/pars_env.c
+++ src/parsing/parse_debug_mode/pars_env.c
@@ -7,15 +7,6 @@
 
 #include "proto_tetris.h"
 
-static char *correct_buffer(char *buffer)
-{
-    char *full_buffer = malloc(sizeof(char) * my_lenght(buffer));
-
-    for (int i = 0; buffer[i] != '-'; i++)
-        full_buffer[i] = buffer[i];
-    return full_buffer;
-}
-
 char *parsing_env(char **env, char *path)
 {
     char *buffer = NULL;
@@ -24,9 +15,8 @@ char *parsing_env(char **env, char *path)
     for (int y = 0; env[y]; y++) {
         counter = my_strncmp(env[y], path, my_lenght(path));
         if (counter == 0) {
-            buffer = env[y] + 5;
+            buffer = strdup(env[y] + 5);
         }
     }
-    buffer = correct_buffer(buffer);
     return buffer;
 }
