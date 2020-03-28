d74ce321c7c6e11439f163d6f5c0a9ee10c8e2a8
Liam <liam.esteffe@epitech.eu>
2020-03-28 01:12:45 +0100

    <feat> adding error handling

--- src/game/debug_mode/without_spaces.c
+++ src/game/debug_mode/without_spaces.c
@@ -7,6 +7,32 @@
 
 #include "proto_tetris.h"
 
+static void too_long_lenght(tetris_t *tetris)
+{
+    for (int j = 0; AV[j]; j++) {
+        for (int i = 0; AV[j][i]; i++) {
+            if (AV[j][i] == '-' && AV[j][i + 1] != 'D' && my_lenght(AV[j]) <= 2\
+            && AV[j][i + 2] != '=') {
+                j += 1;
+                if (my_lenght(AV[j]) >= 2)
+                    help_mode(AV, 84);
+            }
+        }
+    }
+}
+
+static void check_after_equal2(tetris_t *tetris)
+{
+    for (int j = 0; AV[j]; j++) {
+        for (int i = 0; AV[j][i]; i++) {
+            if (AV[j][i] == '-') {
+                if (AV[j][i + 1] != '-' && AV[j][i + 2] == '=')
+                    help_mode(AV, 84);
+            }
+        }
+    }
+}
+
 void without_spaces(tetris_t *tetris)
 {
     for (int i = 0; i < 7; i++) {
@@ -20,9 +46,11 @@ void space_after_equal(tetris_t *tetris)
     for (int j = 0; AV[j]; j++) {
         for (int i = 0; AV[j][i]; i++) {
             if (AV[j][i] == '=') {
-                if (AV[j][i + 1] == '\0')
+                if (AV[j][i + 1] == '\0' || AV[j][i + 2] != '\0')
                     help_mode(AV, 84);
             }
         }
     }
+    check_after_equal2(tetris);
+    too_long_lenght(tetris);
 }
