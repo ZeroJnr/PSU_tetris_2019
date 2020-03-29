a07d825c6ff356ab9e88881a8127448ab55d3a74
martin.le-morvan@epitech.eu <martin.le-morvan@epitech.eu@epitech.eu>
2020-03-29 19:01:12 +0200

    error_tetri

---
+++ src/parsing/parse_tetriminos/error_tetri.c
@@ -0,0 +1,82 @@
+/*
+** EPITECH PROJECT, 2020
+** error_tetri.c
+** File description:
+** error_tetri
+*/
+
+#include "proto_tetris.h"
+
+static void disp_error(char *filepath)
+{
+    my_putstr("Tetriminos : Name ");
+    get_name(filepath);
+    my_putstr(" : Error\n");
+}
+
+static int check_car(char *buffer, char *filepath)
+{
+    int i = 0;
+
+    for (; buffer[i] != '\n'; ++i);
+    i = i + 1;
+    for (; buffer[i] != '\0'; ++i) {
+        if (CHAR) {
+            disp_error(filepath);
+            return 84;
+        }  
+    }
+    return 0;
+}
+
+static int check_lines(char *buffer, char *filepath)
+{
+    int count = 0;
+    int i = 0;
+
+    for (; buffer[i] != '\n'; ++i);
+    ++i;
+    for (; buffer[i] != '\0'; ++i) {
+        count++;
+        if (buffer[i] == '\n' && count  - 1 == buffer[0] - '0')
+            count = 0;
+        else if (buffer[i] == '\n' && count - 1 != buffer[0] - '0') {
+            disp_error(filepath);
+            return 84;
+        }
+    }
+    return 0;
+}
+
+static int check_cols(char *buffer, char *filepath)
+{
+    int count = 0;
+    int i = 0;
+
+    for (; buffer[i] != '\0'; ++i) {
+        if (buffer[i] == '\n')
+            count++;
+    }
+    if (count - 1 != buffer[2] - '0') {
+        disp_error(filepath);
+        return 84;
+    }
+    return 0;
+}
+
+int error_tetri(char *buffer, char *filepath)
+{
+    for (int i = 0; buffer[i] != '\n'; ++i) {
+        if ((buffer[i] < '0' || buffer[i] > '9') && buffer[i] != ' ') {
+            disp_error(filepath);
+            return 84;
+        }
+    }
+    if (check_cols(buffer, filepath) == 84)
+        return 84;
+    else if (check_lines(buffer, filepath) == 84)
+        return 84;
+    if (check_car(buffer, filepath) == 84)
+        return 84;
+    return 0;
+}
